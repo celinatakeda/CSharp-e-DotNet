57b0efbcc0f00c2c8729804e0fe0af1f0516187e
Celina Takeda <takedacarina@gmail.com>
2022-01-16 10:39:36 -0300

    commit

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Primeiro;
+using Interface;
+using Enum;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            var pessoa = (Pessoas)0;
+            Pessoas pessoa2 = Pessoas.João;
+            Pessoas pessoa3 = (Pessoas)4;
+
+            Animal animal = new Animal();
+
+            animal.Nome = "Rex";
+            animal.NomeDono = "Pedro";
+            animal.Especie = "Cachorro";
+
+            Pessoa person = new Pessoa();
+
+            person.Nome = "Celina";
+            person.Estado = "SP";
+            person.Idade = 48;
+
+            var person2 = new Pessoa();
+
+            person2.Nome = "Julia";
+            person2.Estado = "PR";
+            person2.Idade = 28;           
+
+            var classe = new Classe();
+            var classe2 = new Segundo.Classe();
+
+            Console.WriteLine(pessoa3);
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
+
---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            var a = "Hello ";
+            var b = "World!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
