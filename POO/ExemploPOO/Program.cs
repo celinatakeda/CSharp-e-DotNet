c8eb013685b17e2cfb98cd6874e14dc7991b3cc0
Celina Takeda <takedacarina@gmail.com>
2022-01-17 09:54:10 -0300

    commit

--- POO/ExemploPOO/Program.cs
+++ POO/ExemploPOO/Program.cs
@@ -7,12 +7,33 @@ namespace ExemploPOO
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa();
-
+            // Herança
+            Professor p1 = new Professor();
+            p1.Salario = 2000;
             p1.Nome = "Bob";
             p1.Idade = 20;
+            p1.Documento = "123456";
 
             p1.Apresentar();
+
+            // Encapsualamento
+            // Valores válidos
+            // Retangulo r = new Retangulo();
+            // r.DefinirMedidas(30, 30);
+            // System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+            // // Valores inválidos
+            // Retangulo r2 = new Retangulo();
+            // r2.DefinirMedidas(0, 0);
+            // System.Console.WriteLine($"Área: {r2.ObterArea()}");
+
+            // Abstração
+            // Pessoa p1 = new Pessoa();
+
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+
+            // p1.Apresentar();
         }
     }
 }
