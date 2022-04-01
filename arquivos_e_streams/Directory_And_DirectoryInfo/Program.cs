702b3f5cce848f01c8b78cfb1b86140b615c072a
Celina Takeda <takedacarina@gmail.com>
2022-04-01 16:37:37 -0300

    commit

---
+++ arquivos_e_streams/Directory_And_DirectoryInfo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var path = Path.Combine(Environment.CurrentDirectory, "globo");
+var dirGlobo = Directory.CreateDirectory(path);
+var dirAmNorte = dirGlobo.CreateSubdirectory("América do Norte");
+var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+var dirAmSul = dirGlobo.CreateSubdirectory("América do Sul");
+
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("Mexico");
+dirAmNorte.CreateSubdirectory("Canada");
+
+dirAmCentral.CreateSubdirectory("Costa Rica");
+dirAmCentral.CreateSubdirectory("Panama");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Argentina");
+dirAmSul.CreateSubdirectory("Paraguai");
+
+
