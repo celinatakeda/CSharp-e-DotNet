29bc8d8e15e1854540c1293b424219b29997b0d7
Celina Takeda <takedacarina@gmail.com>
2022-04-01 12:16:48 -0300

    commit

---
+++ arquivos_e_streams/File_And_FileInfo/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+
+WriteLine("Digite o nome do arquivo:");
+var nome = ReadLine();
+
+nome = LimparNome(nome);
+
+var path = Path.Combine(Environment.CurrentDirectory,$"{nome}.txt");
+
+CriarArquivo(path);
+
+WriteLine("Digite enter para finalizar...");
+ReadLine();
+
+static string LimparNome(string nome)
+{
+    foreach (var @char in Path.GetInvalidFileNameChars())
+    {
+        nome = nome.Replace(@char, '-');
+    }
+    return nome;
+}
+
+// criar arquivo e escrevendo no arquivo teste.txt
+static void CriarArquivo(string path)
+{
+    try
+    {
+        using var sw = File.CreateText(path); //using descarrega o conteúdo no final 
+        sw.WriteLine("Está a linha 1 do arquivo");
+        sw.WriteLine("Está a linha 2 do arquivo");
+        sw.WriteLine("Está a linha 3 do arquivo");
+        //sw.Flush(); //descarrega o conteúdo para o arquivo teste.txt
+    }
+    catch
+    {
+        WriteLine("O nome do arquivo está inválido!");
+    }
+
+}
+
+// criando arquivo teste.txt
+//File.Create(path);
